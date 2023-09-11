f7fa0993be9368d0bbd3b13fa23e589dc81e14d4
Teo Baranga <teobaranga@users.noreply.github.com>
2023-09-11 16:01:14 +0100

    add extended material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.androidx.compose.material.icons)
 
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
