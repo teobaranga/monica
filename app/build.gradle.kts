5a0135d67eb0a6e2656056a7ca451791a31755dd
Teo Baranga <teobaranga@users.noreply.github.com>
2023-10-11 21:19:44 +0100

    release: enable minifying
    Use R8 to shrink code & resources.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
             applicationIdSuffix = ".debug"
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
