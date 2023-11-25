42f1a6696cacb8f96bc19ff1671722efab4bed8e
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2023-11-25 20:42:15 +0000

    journal: add animations

--- app/src/main/java/com/teobaranga/monica/journal/view/JournalEntry.kt
+++ app/src/main/java/com/teobaranga/monica/journal/view/JournalEntry.kt
@@ -1,15 +1,25 @@
 package com.teobaranga.monica.journal.view
 
 import JournalNavGraph
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.spec.DestinationStyle
 import com.teobaranga.monica.journal.view.ui.JournalEntryScreen
 
 @JournalNavGraph
-@Destination
+@Destination(
+    style = JournalEntryTransitions::class,
+)
 @Composable
 fun JournalEntry(
     entryId: Int? = null,
@@ -19,4 +29,15 @@ fun JournalEntry(
     JournalEntryScreen(
         entry = entry,
     )
+}
+
+object JournalEntryTransitions : DestinationStyle.Animated {
+
+    override fun AnimatedContentTransitionScope<NavBackStackEntry>.enterTransition(): EnterTransition {
+        return fadeIn(animationSpec = tween(300))
+    }
+
+    override fun AnimatedContentTransitionScope<NavBackStackEntry>.exitTransition(): ExitTransition {
+        return fadeOut(animationSpec = tween(300))
+    }
 }
