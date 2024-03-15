63b143aaeaa983f1cead1a33993e7ee4cd7f8cb7
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2024-03-15 00:19:23 +0000

    journal: continue implementing view/edit screen

--- app/src/main/java/com/teobaranga/monica/journal/view/ui/JournalEntryScreen.kt
+++ app/src/main/java/com/teobaranga/monica/journal/view/ui/JournalEntryScreen.kt
@@ -1,17 +1,33 @@
 package com.teobaranga.monica.journal.view.ui
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.teobaranga.monica.journal.model.JournalEntry
 import com.teobaranga.monica.ui.PreviewPixel4
@@ -19,46 +35,91 @@ import com.teobaranga.monica.ui.theme.MonicaTheme
 import java.time.OffsetDateTime
 
 @Composable
+fun keyboardAsState(): State<Boolean> {
+    val isImeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+    return rememberUpdatedState(isImeVisible)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun JournalEntryScreen(entry: JournalEntry?, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .statusBarsPadding()
-            .imePadding(),
-    ) {
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp)
-                .padding(horizontal = 16.dp),
-            value = entry?.title.orEmpty(),
-            onValueChange = {
-                // TODO
-            },
-            textStyle = MaterialTheme.typography.bodyMedium,
-            placeholder = {
-                Text(
-                    text = "Title",
-                )
-            },
-            maxLines = 1,
-        )
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            value = entry?.post.orEmpty(),
-            onValueChange = {
-                // TODO
-            },
-            textStyle = MaterialTheme.typography.bodyMedium,
-            placeholder = {
-                Text(
-                    text = "How was your day?",
-                )
-            },
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+
+                        },
+                    ) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                    }
+                },
+                title = {
+                    Text("Journal Entry")
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+
+                        },
+                    ) {
+                        Icon(Icons.Filled.Check, "Add a new entry")
+                    }
+                }
+            )
+        },
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .padding(paddingValues)
+                .consumeWindowInsets(paddingValues)
+                .imePadding(),
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                value = entry?.title.orEmpty(),
+                onValueChange = {
+                    // TODO
+                },
+                textStyle = MaterialTheme.typography.bodyMedium,
+                placeholder = {
+                    Text(
+                        text = "Title",
+                    )
+                },
+                maxLines = 1,
+                shape = StartVerticalLineShape,
+            )
+            val scrollState = rememberScrollState()
+            val keyb by keyboardAsState()
+            LaunchedEffect(keyb) {
+                if (keyb) {
+                    scrollState.scrollBy(20f)
+                }
+            }
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(16.dp)
+                    .verticalScroll(scrollState),
+                value = entry?.post.orEmpty(),
+                onValueChange = {
+                    // TODO
+                },
+                textStyle = MaterialTheme.typography.bodyMedium,
+                placeholder = {
+                    Text(
+                        text = "How was your day?",
+                    )
+                },
+                shape = StartVerticalLineShape,
+            )
+        }
     }
 }
 
