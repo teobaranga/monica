db3eecd5ee03cfc00bbe463f613c5d0833a738e7
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2024-06-29 11:40:26 +0100

    feat(theme): make month/day formatter a Composable
    This should make it a bit easier to override down the Composable tree.

--- app/src/main/java/com/teobaranga/monica/ui/datetime/LocalDateTimeFormatter.kt
+++ app/src/main/java/com/teobaranga/monica/ui/datetime/LocalDateTimeFormatter.kt
@@ -1,11 +1,12 @@
 package com.teobaranga.monica.ui.datetime
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalConfiguration
 import java.time.chrono.Chronology
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 import java.time.format.FormatStyle
-import java.util.Locale
 
 val LocalDateFormatter = staticCompositionLocalOf<DateTimeFormatter> {
     DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
@@ -15,16 +16,14 @@ val LocalMonthDayFormatter = staticCompositionLocalOf<DateTimeFormatter> {
     error("CompositionLocal LocalMonthDayFormatter not present")
 }
 
-fun getDefaultMonthDayFormatter(locale: Locale): DateTimeFormatter {
+@Composable
+fun getMonthDayFormatter(dateStyle: FormatStyle = FormatStyle.MEDIUM): DateTimeFormatter {
+    val locale = LocalConfiguration.current.locales[0]
     val format = DateTimeFormatterBuilder.getLocalizedDateTimePattern(
-        /* dateStyle = */
-        FormatStyle.MEDIUM,
-        /* timeStyle = */
-        null,
-        /* chrono = */
-        Chronology.ofLocale(locale),
-        /* locale = */
-        locale,
+        /* dateStyle = */ dateStyle,
+        /* timeStyle = */ null,
+        /* chrono = */ Chronology.ofLocale(locale),
+        /* locale = */ locale,
     ).filterNot {
         // Remove year
         val lowercaseChar = it.lowercaseChar()
