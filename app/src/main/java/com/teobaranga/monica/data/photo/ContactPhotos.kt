38a1bc1505d9a347a9012df8fe7ee080123e36ce
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2024-08-21 22:57:52 +0100

    fix(contacts): match avatarUrl to column

--- app/src/main/java/com/teobaranga/monica/data/photo/ContactPhotos.kt
+++ app/src/main/java/com/teobaranga/monica/data/photo/ContactPhotos.kt
@@ -1,9 +1,11 @@
 package com.teobaranga.monica.data.photo
 
+import androidx.room.ColumnInfo
 import androidx.room.Relation
 
 data class ContactPhotos(
     val contactId: Int,
+    @ColumnInfo("avatar_url")
     val avatarUrl: String?,
     @Relation(parentColumn = "contactId", entityColumn = "contactId")
     val photos: List<PhotoEntity>,
