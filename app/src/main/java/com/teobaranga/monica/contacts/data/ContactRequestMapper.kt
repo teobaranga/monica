9ca744b1572a106979489df39b1fb5cdbe73fb87
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2024-09-07 17:06:06 +0100

    feat(birthday): support fully known birthdays

--- app/src/main/java/com/teobaranga/monica/contacts/data/ContactRequestMapper.kt
+++ app/src/main/java/com/teobaranga/monica/contacts/data/ContactRequestMapper.kt
@@ -13,16 +13,32 @@ class ContactRequestMapper @Inject constructor() {
             lastName = entity.lastName,
             nickname = entity.nickname,
             genderId = 1,
-            birthdateDay = null,
-            birthdateMonth = null,
-            birthdateYear = null,
+            birthdateDay = entity.getBirthdateDay(),
+            birthdateMonth = entity.getBirthdateMonth(),
+            birthdateYear = entity.getBirthdateYear(),
             isBirthdateKnown = entity.birthdate != null,
             birthdateIsAgeBased = entity.birthdate?.isAgeBased ?: false,
-            birthdateAge = entity.birthdate?.run {
-                ChronoUnit.YEARS.between(date, OffsetDateTime.now()).toInt()
-            },
+            birthdateAge = entity.getBirthdateAge(),
             isDeceased = false,
             isDeceasedDateKnown = false,
         )
+    }
+
+    private fun ContactEntity.getBirthdateDay(): Int? {
+        return birthdate?.date?.dayOfMonth
+    }
+
+    private fun ContactEntity.getBirthdateMonth(): Int? {
+        return birthdate?.date?.monthValue
+    }
+
+    private fun ContactEntity.getBirthdateYear(): Int? {
+        return birthdate?.date?.year
+    }
+
+    private fun ContactEntity.getBirthdateAge(): Int? {
+        return birthdate?.date?.run {
+            ChronoUnit.YEARS.between(this, OffsetDateTime.now()).toInt()
+        }
     }
 }
