f775580b24ae13e6f335940035740c31aff17eac
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2025-04-30 20:10:54 +0100

    remove room-ktx
    The dependency is empty and merged into room-runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,7 +110,6 @@ dependencies {
     implementation(libs.paging.compose)
 
     implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
     ksp(libs.room.compiler)
 
     implementation(libs.kmlogging)
