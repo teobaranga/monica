633f6601b6846ccad2f18a598ffcca5337f3a1d9
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2025-10-04 14:46:31 +0100

    start redesigning participants section

--- app/src/commonMain/kotlin/com/teobaranga/monica/contacts/detail/activities/edit/ui/EditContactActivityUiState.kt
+++ app/src/commonMain/kotlin/com/teobaranga/monica/contacts/detail/activities/edit/ui/EditContactActivityUiState.kt
@@ -2,11 +2,12 @@ package com.teobaranga.monica.contacts.detail.activities.edit.ui
 
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.annotation.RememberInComposition
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import kotlinx.coroutines.flow.StateFlow
+import androidx.compose.runtime.snapshots.Snapshot
 import kotlinx.datetime.LocalDate
 
 sealed interface EditContactActivityUiState {
@@ -14,19 +15,33 @@ sealed interface EditContactActivityUiState {
     data object Loading : EditContactActivityUiState
 
     @Stable
-    data class Loaded(
-        private val initialDate: LocalDate,
-        val participantResults: StateFlow<List<ActivityParticipant>>,
-    ) : EditContactActivityUiState {
+    class Loaded(initialDate: LocalDate) : EditContactActivityUiState {
 
-        var date: LocalDate by mutableStateOf(initialDate)
+        var date by mutableStateOf(initialDate)
 
-        val participantSearch = TextFieldState()
-
-        val participants = mutableStateListOf<ActivityParticipant.Contact>()
+        val participantsState = ParticipantsState()
 
         val summary = TextFieldState()
 
         val details = TextFieldState()
+    }
+}
+
+@Stable
+class ParticipantsState @RememberInComposition constructor() {
+
+    val participantSearch = TextFieldState()
+
+    val suggestions = mutableStateListOf<ActivityParticipant>()
+
+    val participants = mutableStateListOf<ActivityParticipant.Contact>()
+
+    override fun toString(): String {
+        return Snapshot.withoutReadObservation {
+            "ParticipantsState(" +
+                "participantSearch=\"$participantSearch\", " +
+                "suggestions=$suggestions, " +
+                "participants=$participants)"
+        }
     }
 }
