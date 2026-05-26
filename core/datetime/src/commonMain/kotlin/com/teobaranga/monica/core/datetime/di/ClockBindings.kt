3fe2864e5ad7751fbe6620e6e6c65994105c92b7
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2026-05-26 19:15:15 +0100

    clock: convert to bindingcontainer

--- core/datetime/src/commonMain/kotlin/com/teobaranga/monica/core/datetime/di/ClockBindings.kt
+++ core/datetime/src/commonMain/kotlin/com/teobaranga/monica/core/datetime/di/ClockBindings.kt
@@ -1,12 +1,14 @@
 package com.teobaranga.monica.core.datetime.di
 
 import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
 import kotlin.time.Clock
 
 @ContributesTo(AppScope::class)
-interface ClockComponent {
+@BindingContainer
+object ClockBindings {
 
     @Provides
     fun clock(): Clock = Clock.System
--- core/test/src/commonMain/kotlin/com/teobaranga/monica/core/test/TestClockBindings.kt
+++ core/test/src/commonMain/kotlin/com/teobaranga/monica/core/test/TestClockBindings.kt
@@ -1,7 +1,8 @@
 package com.teobaranga.monica.core.test
 
-import com.teobaranga.monica.core.datetime.di.ClockComponent
+import com.teobaranga.monica.core.datetime.di.ClockBindings
 import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
 import kotlin.time.Clock
@@ -11,9 +12,10 @@ val testNow = Instant.parse("2025-01-01T00:00:00Z")
 
 @ContributesTo(
     scope = AppScope::class,
-    replaces = [ClockComponent::class],
+    replaces = [ClockBindings::class],
 )
-interface TestClockComponent {
+@BindingContainer
+object TestClockBindings {
 
     @Provides
     fun clock(): Clock {
