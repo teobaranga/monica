3d13d094874ba4b2e49d89033603d28ba8f85d3c
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2026-05-26 19:12:10 +0100

    journal: fix warnings

--- feature/journal/src/androidHostTest/kotlin/com/teobaranga/monica/journal/TestJournalComponent.kt
+++ feature/journal/src/androidHostTest/kotlin/com/teobaranga/monica/journal/TestJournalComponent.kt
@@ -1,15 +1,19 @@
 package com.teobaranga.monica.journal
 
+import com.teobaranga.monica.account.AccountViewModel
 import com.teobaranga.monica.component.tips.TipsRepository
 import com.teobaranga.monica.journal.data.local.JournalDao
 import com.teobaranga.monica.journal.data.remote.JournalApi
 import com.teobaranga.monica.journal.view.JournalEntryViewModel
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
-import dev.zacsweers.metro.SingleIn
 
-@DependencyGraph(AppScope::class)
-@SingleIn(AppScope::class)
+@DependencyGraph(
+    scope = AppScope::class,
+    excludes = [
+        AccountViewModel::class,
+    ],
+)
 interface TestJournalComponent {
 
     val journalEntryViewModelFactory: JournalEntryViewModel.Factory
